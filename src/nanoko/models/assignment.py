38da7fd93f89415dcd9b085147fb3fcfe48bbe02
zhaomaoniu <2667292003@qq.com>
2025-06-06 12:30:23 +1200

    fix: add due_date to assignment model

--- src/nanoko/models/assignment.py
+++ src/nanoko/models/assignment.py
@@ -1,3 +1,5 @@
+from typing import Optional
+from datetime import datetime
 from pydantic import BaseModel
 
 
@@ -8,6 +10,7 @@ class Assignment(BaseModel):
     name: str
     description: str
     teacher_id: int
+    due_date: Optional[datetime] = None
 
 
 class Class(BaseModel):
