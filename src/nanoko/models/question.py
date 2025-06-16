df7f05206daf48e325130987338dab060eaa8825
zhaomaoniu <2667292003@qq.com>
2025-06-16 23:55:32 +1200

    chore: remove unused import

--- src/nanoko/models/question.py
+++ src/nanoko/models/question.py
@@ -1,6 +1,6 @@
 from enum import Enum
 from typing import List, Optional
-from pydantic import BaseModel, ConfigDict, model_serializer
+from pydantic import BaseModel, model_serializer
 
 
 # Unsolvable circular import
